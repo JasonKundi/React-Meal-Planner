1f29f8fa01d95534ac09deaa299cfa0718b79588
Jason Kundi <94011909+JasonKundi@users.noreply.github.com>
2022-06-21 15:10:14 +0100

    Project set up, calorie button

---
+++ meal-planner/src/App.js
@@ -0,0 +1,40 @@
+import {useState} from 'react';
+
+function App() {
+  const [mealData, setMealData] = useState(null);
+  const [calories, setCalories] = useState(2000);
+
+  const getMealData = () => {
+    fetch(
+      `https://api.spoonacular.com/mealplanner/generate?apiKey=eca293f96dc34bddbbbf1b26adfa7c61&timeFrame=day&targetCalories=${calories}`
+      )
+      .then(res => res.json())
+      .then(data => {
+        setMealData(data)
+      })
+      .catch(() => {
+        console.log('Error')
+      })
+
+  }
+
+  const handleChange = (e)=> {
+    setCalories(e.target.value)
+
+  }
+
+  return (
+    <div className="App">
+      <section className='controls'>
+        <input
+        type='number'
+        placeholder='Calories (e.g 2000)' 
+        onChange={handleChange}
+        />
+        <button onClick={getMealData}>Get Daily Meal Plan</button>
+      </section>
+    </div>
+  );
+}
+
+export default App;
