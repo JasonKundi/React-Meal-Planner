a39bfff9101ff5a1950aba8ad5e18064f17e544c
Jason Kundi <94011909+JasonKundi@users.noreply.github.com>
2022-06-21 15:12:44 +0100

    fetch call to recieve meal plans on button click

--- meal-planner/src/App.js
+++ meal-planner/src/App.js
@@ -11,6 +11,7 @@ function App() {
       .then(res => res.json())
       .then(data => {
         setMealData(data)
+        console.log(data)
       })
       .catch(() => {
         console.log('Error')
